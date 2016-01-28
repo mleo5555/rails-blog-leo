b1f760e452ba5f49aeb8d3deb951b54c906aa1c8
Matt Leo <mleo5555@gmail.com>
2016-01-28 16:27:22 -0600

    Add controllers, route resources, and new began posts index page

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
